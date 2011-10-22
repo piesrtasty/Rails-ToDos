c65995eac62d93690babb92099f15981312d5aeb
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-22 01:32:49 -0400

    Completely rewrote task index view

--- app/assets/javascripts/views/tasks/tasks_index.js
+++ app/assets/javascripts/views/tasks/tasks_index.js
@@ -1,43 +1,26 @@
-// ExampleApp.Views.TasksIndex = Backbone.View.extend({
-// 	
-// 	initialize: function()	{
-// 		this.render();
-// 	},
-// 	
-// 	render: function()	{
-// 		$(this.el).html(JST['tasks/index']({ tasks: ExampleApp.tasks }));
-// 		
-// 		var tasksIndexView = this;
-// 		ExampleApp.tasks.each(function(task)	{
-// 			var taskView = new ExampleApp.Views.TaskView({model: task});
-// 			tasksIndexView.$('table').append(taskView.render().el)
-// 		});
-// 		
-// 		
-// 		
-// 		$('body').html(this.el);
-// 		
-// 		return this;
-// 	}
-// 
-// })
-
 ExampleApp.Views.TasksIndex = Backbone.View.extend({
-	template: JST['tasks/tasks_index'],
-	tagName: 'section',
-	id: 'tasks',
-
 	
 	initialize: function()	{
 		_.bindAll(this, "render");
-		this.collection.bind("change", this.render);
 		this.collection.bind("add", this.render);
-		this.collection.bind("remove", this.render);
-
 	},
 	
 	render: function()	{
-		$(this.el).html(this.template({tasks: this.collection}))
+		this.renderTemplate();
+		this.renderTasks();
+		return this;
+	},
+	
+	renderTemplate: function()	{
+		$(this.el).html(JST['tasks/index']({ tasks: this.collection }));
+	},
+	
+	renderTasks: function()	{
+		var self = this;
+		this.collection.each(function(task)	{
+			var row = new ExampleApp.View.TaskItem({ model: task });
+			self.$('tbody').append(row.render().el);
+		});
 	}
 	
-});
+})
