105910187c87332f8d5038d8557fcfb4f51386f3
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-22 10:35:54 -0400

    branching book

--- db/migrate/20111022055855_add_task_complete.rb
+++
@@ -1,9 +0,0 @@
-class AddTaskComplete < ActiveRecord::Migration
-  def up
-    add_column :tasks, :complete, :boolean, :default => false, :null => false
-  end
-
-  def down
-    remove_column :tasks, :complete
-  end
-end
