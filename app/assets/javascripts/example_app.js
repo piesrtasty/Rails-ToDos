e5bf7846cb87b91e8599bd5cff8926ce7b23eda5
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-22 00:55:02 -0400

    Aded isComplete() method to Task model to return tasks that have been completed

--- app/assets/javascripts/example_app.js
+++ app/assets/javascripts/example_app.js
@@ -1,20 +1,3 @@
-// var ExampleApp = {
-// 	Models: {},
-// 	Collections: {},
-// 	Views: {},
-// 	Routers: {},
-// 	init: function(tasks)	{
-// 		new ExampleApp.Routers.Tasks();
-// 		this.tasks = new ExampleApp.Collections.Tasks(tasks);
-// 		if (!Backbone.history.started)	{
-// 		  Backbone.history.start();
-// 			Backbone.history.started = true;
-// 		}
-// 		
-// 	alert(tasks[0]['title']);
-// 	
-// 	}
-// };
 
 var ExampleApp = {
 	Models: {},
