a6ed311a70ab2285c2ac4eee9ce451590f50ab4f
virtually-ghetto <lukemhamilton@gmail.com>
2011-10-24 12:04:43 -0400

    Rewrote task_detail.js view

--- app/assets/javascripts/views/task_detail.js
+++ app/assets/javascripts/views/task_detail.js
@@ -1,29 +1,61 @@
-ExampleApp.Views.TaskDetail = Backbone.View.extend({
-	// template: JST['tasks/tasks_detail'], // TODO create template file
+// ExampleApp.Views.TaskDetail = Backbone.View.extend({
+// 	// template: JST['tasks/tasks_detail'], // TODO create template file
+// 	tagName: 'section',
+// 	id: 'task',
+// 	
+// 	events: {
+// 		"click .comments .form-inputs button": "createComment"
+// 	},
+// 	
+// 	initialize: function()	{
+// 		_.bindAll(this, "render");
+// 		
+// 		this.model.bind("change", this.render);
+// 		this.model.comments.bind("change", this.render);
+// 		this.model.comments.bind("add", this.render);
+// 	},
+// 	
+// 	render: function()	{
+// 		$(this.el).html(JST['tasks/tasks_detail']({ task: this.model }));
+// 	},
+// 	
+// 	createComment: function()	{
+// 		var comment = new Comment({ text: this.$('.new-comment-input').val() });
+// 		this.$('.new-comment-input').val('');
+// 		this.model.comments.create(comment);
+// 	}
+// 	
+// 	
+// })
+
+ExampleApp.Views.TaskDetail = Support.CompositeView.extend({
 	tagName: 'section',
 	id: 'task',
 	
-	events: {
-		"click .comments .form-inputs button": "createComment"
-	},
-	
 	initialize: function()	{
-		_.bindAll(this, "render");
-		
-		this.model.bind("change", this.render);
-		this.model.comments.bind("change", this.render);
-		this.model.comments.bind("add", this.render);
+		_.bindAll(this, "renderDetails");
+		this.model.bind("change", this.renderDetails);
 	},
 	
 	render: function()	{
-		$(this.el).html(JST['tasks/tasks_detail']({ task: this.model }));
+		this.renderLayout();
+		this.renderDetail();
+		this.renderCommentsList();
 	},
 	
-	createComment: function()	{
-		var comment = new Comment({ text: this.$('.new-comment-input').val() });
-		this.$('.new-comment-input').val('');
-		this.model.comments.create(comment);
+	renderLayout: function()	{
+		$(this.el).html(JST['tasks/show']());
+	},
+	
+	renderDetails: function()	{
+		var detailsMarkup = JST['tasks/details']({ task: this.model });
+		this.$('.task-details').html(detailsMarkup);
+	},
+	
+	renderCommentsList: function()	{
+		var commentsList = new CommentsList({ model: this.model });
+		var commentsContainer = this.$('comments');
+		this.renderChildInto(commentsList, commentsContainer);
 	}
 	
-	
-})
+});
