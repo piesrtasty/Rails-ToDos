12c22f9f39abc1c39a3793d7a8e9d6f97a3bdca3
virtually-ghetto <lukemhamilton@gmail.com>
2011-11-18 16:16:35 -0500

    Finsiehd task_show.js view

--- app/assets/javascripts/views/task_show.js
+++ app/assets/javascripts/views/task_show.js
@@ -18,6 +18,51 @@ ExampleApp.Views.TaskShow = Backbone.View.extend({
 	
 	renderTemplate: function()	{
 		$(this.el).html(JST['tasks/show']());
-	}
+	},
 	
-})
+	renderTask: function()	{
+		this.$('p').text(this.model.escape('title'));
+		this.$('.upload input').attr('id', 'upload_' + this.model.get('id'));
+		this.$('.upload label').attr('for', 'uload_' + this.model.get('id'));
+	},
+	
+	renderAttachments: function()	{
+		var self = this;
+		var $attachments = this.$('ul.attachments');
+		$attachments.html('');
+		
+		this.model.attachments.each(function(attachment)	{
+			var attachmentView = $('<li><p></p><img></li>');
+			$('p', attachmentView).attr("src", attachment.get('upload_url'));
+			$attachments.append(attachmentView);
+		});
+	},
+	
+	attachUploader: function()	{
+		var uploadUrl = "/tasks/" + this.model.get('id') + '/attachments.json';
+		
+		this.uploader = new uploader(this.uploadInput(),	{
+			url: uploadUrl,
+			success: this.uploadSuccess,
+			prefix: 'upload'
+		});
+		
+		this.uploader.prefilter = function()	{
+			var token = $('meta[name="csrf-token"]').attr('content');
+			if (token) this.xhr.setRequestHeader('X-CSRF-Token', token)
+		}
+	},
+	
+	uploadInput: function()	{
+		return this.$('.upload input').get(0)
+	},
+	
+	upload: function()	{
+		this.uploader.send();
+	},
+	
+	uploadSuccess: function(data)	{
+		this.model.fetch()
+	};
+	
+});
