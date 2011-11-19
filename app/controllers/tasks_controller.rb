c6e6182a885249c05bfb65bade3c9f4ca9785cb9
virtually-ghetto <lukemhamilton@gmail.com>
2011-11-18 21:19:42 -0500

    Created attachments controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,21 @@
 class TasksController < ApplicationController
+  before_filter :authorize
   respond_to :html, :json
 
   def index
-    respond_with(@tasks = Task.all)
+    respond_with(@tasks = current_user.tasks)
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
   end
 
   def create
-    respond_with(Task.create(params[:task]))
+    respond_with(current_user.tasks.create(params[:task]))
   end
 
   def update
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.update_attributes(params[:task])
     respond_with(task)
   end
